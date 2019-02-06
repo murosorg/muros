murosorg/muros

commit b955f93bf53db4c1e99263616ca7fc92e7fa37a8

Util::isPort() validation issue, closes https://github.com/opnsense/core/issues/3189

src/opnsense/mvc/app/library/OPNsense/Firewall/Util.php
@@ -194,9 +194,10 @@ class Util
     {
         $tmp = explode(':', $number);
         foreach ($tmp as $port) {
-            if (!getservbyname($port, "tcp") && !getservbyname($port, "udp")
-                && filter_var($port, FILTER_VALIDATE_INT, array(
+            if (getservbyname($port, "tcp") || getservbyname($port, "udp")
+                || filter_var($port, FILTER_VALIDATE_INT, array(
                     "options" => array("min_range"=>1, "max_range"=>65535))) === false
+                || !is_numeric($port)
             ) {
                 return false;
             }

commit 996fb3f5ba0661ce6466e973fc61d85125de83c4

rework https://github.com/opnsense/core/commit/b955f93bf53db4c1e99263616ca7fc92e7fa37a8 (https://github.com/opnsense/core/issues/3189), accidentally dropped support for known services ;)

src/opnsense/mvc/app/library/OPNsense/Firewall/Util.php
@@ -195,8 +195,9 @@ class Util
         $tmp = explode(':', $number);
         foreach ($tmp as $port) {
             if (!getservbyname($port, "tcp") && !getservbyname($port, "udp")
-                && filter_var($port, FILTER_VALIDATE_INT, array(
-                    "options" => array("min_range"=>1, "max_range"=>65535))) === false
+                && (filter_var($port, FILTER_VALIDATE_INT, array(
+                    "options" => array("min_range"=>1, "max_range"=>65535))
+                  ) === false || !is_numeric($port))
             ) {
                 return false;
             }