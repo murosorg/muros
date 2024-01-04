murosorg/muros

commit b209851b25192a194d5e299e161e4905def72bd9

configd - support streaming output using the "stream_output" handler.
This commit adds the connection to the action.execute() method which offers the ability to flush data back to the client instantly without having to wait for the call to end. As the existing types are only slighly modified, the impact of this change should be rather low. To demonstrate the streaming capability, also adjust configctl to flush output when received.

Example usage:

[find]
command:find / ; xxx
parameters:
type:stream_output
message:test stream output

src/opnsense/service/configd_ctl.py
@@ -57,19 +57,16 @@ def exec_config_cmd(exec_command):
     except socket.error:
         syslog_error('unable to connect to configd socket (@%s)'%configd_socket_name)
         print('unable to connect to configd socket (@%s)'%configd_socket_name, file=sys.stderr)
-        return None
+        yield None
 
     try:
         sock.send(exec_command.encode())
-        data = []
         while True:
             line = sock.recv(65536).decode()
             if line:
-                data.append(line)
+                yield line
             else:
                 break
-
-        return ''.join(data)[:-3]
     except:
         syslog_error('error in configd communication \n%s'%traceback.format_exc())
         print ('error in configd communication %s, see syslog for details', file=sys.stderr)
@@ -144,8 +141,12 @@ else:
     for exec_command in exec_commands:
         if args.d:
             exec_command = '&' + exec_command
-        result=exec_config_cmd(exec_command=exec_command)
-        if result is None:
-            sys.exit(-1)
-        if not args.q:
-            print('%s' % (result.strip()))
+        endmarker = (chr(0), chr(0), chr(0))
+        for block in exec_config_cmd(exec_command=exec_command):
+            if block is None:
+                sys.exit(-1)
+            else:
+                if block.endswith(endmarker):
+                    print(block[:-3].rstrip())
+                else:
+                    print(block, end="")

plist
@@ -1191,6 +1191,7 @@
 /usr/local/opnsense/service/modules/actions/none.py
 /usr/local/opnsense/service/modules/actions/script.py
 /usr/local/opnsense/service/modules/actions/script_output.py
+/usr/local/opnsense/service/modules/actions/stream_output.py
 /usr/local/opnsense/service/modules/addons/__init__.py
 /usr/local/opnsense/service/modules/addons/template_helpers.py
 /usr/local/opnsense/service/modules/config.py

src/opnsense/service/modules/actions/base.py
@@ -72,10 +72,11 @@ class BaseAction:
 
         return script_command
 
-    def execute(self, parameters, message_uuid):
+    def execute(self, parameters, message_uuid, connection):
         """ execute an action
         :param parameters: list of parameters
         :param message_uuid: unique message id
+        :param connection: response socket
         :return:
         """
         # send-out syslog message

src/opnsense/service/modules/actions/inline.py
@@ -36,8 +36,8 @@ class Action(BaseAction):
         self.root_dir = action_parameters.get('root_dir', None)
         self.config = action_parameters.get('config', None)
 
-    def execute(self, parameters, message_uuid):
-        super().execute(parameters, message_uuid)
+    def execute(self, parameters, message_uuid, *args, **kwargs):
+        super().execute(parameters, message_uuid, *args, **kwargs)
         try:
             # match parameters, serialize to parameter string defined by action template
             if len(parameters) > 0:

src/opnsense/service/modules/actions/none.py
@@ -27,6 +27,6 @@ from .base import BaseAction
 
 
 class Action(BaseAction):
-    def execute(self, parameters, message_uuid):
-        super().execute(parameters, message_uuid)
+    def execute(self, parameters, message_uuid, *args, **kwargs):
+        super().execute(parameters, message_uuid, *args, **kwargs)
         return 'Unknown action type'

src/opnsense/service/modules/actions/script.py
@@ -30,8 +30,8 @@ from .base import BaseAction
 
 
 class Action(BaseAction):
-    def execute(self, parameters, message_uuid):
-        super().execute(parameters, message_uuid)
+    def execute(self, parameters, message_uuid, *args, **kwargs):
+        super().execute(parameters, message_uuid, *args, **kwargs)
         try:
             script_command = self._cmd_builder(parameters)
         except TypeError as e:

src/opnsense/service/modules/actions/stream_output.py
@@ -23,37 +23,56 @@
     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
     POSSIBILITY OF SUCH DAMAGE.
 """
-import tempfile
+
 import traceback
+import selectors
 import subprocess
 from .. import syslog_error
 from .base import BaseAction
 
 
 class Action(BaseAction):
-    def execute(self, parameters, message_uuid, *args, **kwargs):
-        super().execute(parameters, message_uuid, *args, **kwargs)
+    def execute(self, parameters, message_uuid, connection, *args, **kwargs):
+        super().execute(parameters, message_uuid, connection, *args, **kwargs)
         try:
             script_command = self._cmd_builder(parameters)
         except TypeError as e:
             return str(e)
 
+        self.stdout_read = 0
+        def stdout_reader(stream, mask):
+            data = stream.read(1024)
+            self.stdout_read += len(data)
+            connection.send(data)
+
+        process = subprocess.Popen(
+            script_command,
+            env=self.config_environment,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        selector = selectors.DefaultSelector()
+        selector.register(process.stdout, selectors.EVENT_READ, stdout_reader)
+
         try:
-            with tempfile.NamedTemporaryFile() as error_stream:
-                with tempfile.NamedTemporaryFile() as output_stream:
-                    subprocess.check_call(script_command, env=self.config_environment, shell=True,
-                                          stdout=output_stream, stderr=error_stream)
-                    output_stream.seek(0)
-                    error_stream.seek(0)
-                    script_output = output_stream.read()
-                    script_error_output = error_stream.read()
-                    if len(script_error_output) > 0:
-                        syslog_error('[%s] Script action stderr returned "%s"' % (
-                            message_uuid, script_error_output.strip()[:255]
-                        ))
-                    return script_output
+            while process.poll() is None:
+                for key, mask in selector.select():
+                    callback = key.data
+                    callback(key.fileobj, mask)
+
+            return_code = process.wait()
+            script_error_output = process.stderr.read().decode()
+            if len(script_error_output) > 0:
+                syslog_error('[%s] Script action stderr returned "%s" (%d)' % (
+                    message_uuid, script_error_output.strip()[:255], return_code
+                ))
+            selector.close()
         except Exception as script_exception:
-            syslog_error('[%s] Script action failed with %s at %s' % (
-                message_uuid, script_exception, traceback.format_exc()
+            syslog_error('[%s] Script action failed with %s at %s (bytes processed %d)' % (
+                message_uuid, script_exception, traceback.format_exc(), self.stdout_read
             ))
             return 'Execute error'
+
+        return None

src/opnsense/service/modules/processhandler.py
@@ -180,11 +180,16 @@ class HandlerClient(threading.Thread):
                     self.action_handler.show_action(data_parts, self.message_uuid)
                     result = 'OK'
                 else:
-                    result = self.action_handler.execute(data_parts, self.message_uuid)
+                    result = self.action_handler.execute(data_parts, self.message_uuid, self.connection)
 
                 if not exec_in_background:
-                    # send response back to client( including trailing enter )
-                    self.connection.sendall(('%s\n' % result).encode())
+                    # send response back to client (including trailing enters)
+                    # ignore when result is None, in which case the content was streamed via the pipe
+                    if type(result) is bytes:
+                        self.connection.sendall(result)
+                        self.connection.sendall(b'\n\n')
+                    elif result is not None:
+                        self.connection.sendall(('%s\n\n' % result).encode())
                 else:
                     # log response
                     syslog_info("message %s [%s] returned %s " % (
@@ -316,7 +321,7 @@ class ActionHandler(object):
 
         return None, []
 
-    def execute(self, action, message_uuid):
+    def execute(self, action, message_uuid, connection=None):
         """ execute configuration defined action
         :param action: list of commands and parameters
         :param message_uuid: message unique id
@@ -325,7 +330,7 @@ class ActionHandler(object):
         action_obj, action_params = self.find_action(action)
 
         if action_obj is not None:
-            return '%s\n' % action_obj.execute(action_params, message_uuid)
+            return action_obj.execute(action_params, message_uuid, connection)
 
         return 'Action not found\n'
 

commit 1c8f0e62ec48b2d82af8d0279ceee6f50d516b87

configctl - regression in https://github.com/opnsense/core/commit/b209851b25192a194d5e299e161e4905def72bd9 , ignoring -q

src/opnsense/service/configd_ctl.py
@@ -145,7 +145,7 @@ else:
         for block in exec_config_cmd(exec_command=exec_command):
             if block is None:
                 sys.exit(-1)
-            else:
+            elif not args.q:
                 if block.endswith(endmarker):
                     print(block[:-3].rstrip())
                 else: