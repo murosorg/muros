murosorg/muros

commit 9eba823c3a5a2c2f2fcab1f023868ef31b901053

mvc: ApiControllerBase->configdStream() - prevent zlib compression as this will add buffering as well, which will result in sluggish behavior for dashboard widgets when webui compression is enabled. (https://github.com/opnsense/core/issues/7244)

src/opnsense/mvc/app/controllers/OPNsense/Base/ApiControllerBase.php
@@ -178,6 +178,9 @@ class ApiControllerBase extends ControllerRoot
         ],
         $poll_timeout = 2
     ) {
+        /* Never allow output compression on streams */
+        ini_set('zlib.output_compression', 'Off');
+        ob_end_clean();
         $response = (new Backend())->configdpStream($action, $params, $poll_timeout);
         foreach ($headers as $header) {
             header($header);

commit ed02bded42dbd5e9e86060074bb2a5904bee106d

mvc: configdStream: minor regression in https://github.com/opnsense/core/commit/9eba823c3a5a2c2f2fcab1f023868ef31b901053
If zlib compression was never enabled to begin with, the ob_end_clean()
will close the current Phalcon output buffer, which means the
ob_end_flush() will cause a fatal error. To prevent this we can check
if there is an output buffer still active.

src/opnsense/mvc/app/controllers/OPNsense/Base/ApiControllerBase.php
@@ -185,7 +185,9 @@ class ApiControllerBase extends ControllerRoot
         foreach ($headers as $header) {
             header($header);
         }
-        ob_end_flush();
+        while (ob_get_level() > 0) {
+            ob_end_flush();
+        }
         fpassthru($response);
     }
 