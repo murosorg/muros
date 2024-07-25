murosorg/muros

commit a89f5610f68bc9fa0bf8d8cc978ec84e607ab8f5

mvc: remove some stray code in api handling for https://github.com/opnsense/core/issues/6389.
Fixes:

   Error: Call to undefined method OPNsense\Mvc\Dispatcher::getParams() in /usr/local/opnsense/mvc/app/controllers/OPNsense/Base/ApiControllerBase.php:284

dispatch errors are handled inside the Dispatch->resolve() now

src/opnsense/mvc/app/controllers/OPNsense/Base/ApiControllerBase.php
@@ -272,37 +272,9 @@ class ApiControllerBase extends ControllerRoot
                                 $this->response->send();
                                 return false;
                             } else {
-                                // authentication + authorization successful.
-                                // pre validate request and communicate back to the user on errors
-                                $callMethodName = $dispatcher->getActionName() . 'Action';
-                                $dispatchError = null;
-                                // check number of parameters using reflection
-                                $object_info = new \ReflectionObject($this);
-                                if ($object_info->hasMethod($callMethodName)) {
-                                    // only inspect parameters if object exists
-                                    $req_c = $object_info->getMethod($callMethodName)->getNumberOfRequiredParameters();
-                                    if ($req_c > count($dispatcher->getParams())) {
-                                        $dispatchError = 'action ' . $dispatcher->getActionName() .
-                                            ' expects at least ' . $req_c . ' parameter(s)';
-                                    }
-                                }
-                                // if body is send as json data, parse to $_POST first
-                                $dispatchError = empty($dispatchError) ? $this->parseJsonBodyData() : $dispatchError;
-
-                                if ($dispatchError != null) {
-                                    // send error to client
-                                    $this->response->setStatusCode(400, "Bad Request");
-                                    $this->response->setContentType('application/json', 'UTF-8');
-                                    $this->response->setJsonContent(
-                                        array('message' => $dispatchError,
-                                            'status'  => 400)
-                                    );
-                                    $this->response->send();
-                                    return false;
-                                }
-
                                 // link username on successful login
                                 $this->logged_in_user = $authResult['username'];
+
                                 // pass revision context to config object
                                 Config::getInstance()->setRevisionContext([
                                     'username' => $authResult['username'],

commit 63288e6a0b3212081c844591ea1f2cceca1ed5c8

MVC: fix API regression caused by https://github.com/opnsense/core/commit/a89f5610f68bc9fa0bf8d8cc978ec84e607ab8f5, closes https://github.com/opnsense/core/issues/7645

src/opnsense/mvc/app/controllers/OPNsense/Base/ApiControllerBase.php
@@ -274,6 +274,15 @@ class ApiControllerBase extends ControllerRoot
                             } else {
                                 // link username on successful login
                                 $this->logged_in_user = $authResult['username'];
+                                // if body is send as json data, parse to $_POST first
+                                $dispatchError = $this->parseJsonBodyData();
+                                if ($dispatchError != null) {
+                                    $this->response->setStatusCode(400, "Bad Request");
+                                    $this->response->setContentType('application/json', 'UTF-8');
+                                    $this->response->setJsonContent(['status'  => 400, 'message' => $dispatchError]);
+                                    $this->response->send();
+                                    return false;
+                                }
 
                                 // pass revision context to config object
                                 Config::getInstance()->setRevisionContext([