murosorg/muros

commit 700d1d65ea4da9fc34b52e8c3fb6585b33f6adf9

mvc: BaseListField - always map values in getDescription() and prevent usage of getNodeOptions() when not needed which should be less work for the cpu.
If a leaf object wants to render its own description, it can still do so by overwriting getDescription(), the cases we have implemented just trust the data of the BaseListField at the moment anyway.

closes https://github.com/opnsense/core/issues/9127

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseListField.php
@@ -134,18 +134,11 @@ abstract class BaseListField extends BaseField
      */
     public function getDescription()
     {
-        $data = $this->getNodeOptions();
-        if (is_array($data)) {
-            $items = [];
-            foreach ($data as $fieldValue) {
-                if ($fieldValue['selected'] == 1) {
-                    $items[] = $fieldValue['value'];
-                }
-            }
-            return implode(', ', $items);
-        } else {
-            return $data;
+        $items = [];
+        foreach ($this->getValues() as $item) {
+            $items[] = $this->internalOptionList[$item] ?? $item;
         }
+        return implode(', ', $items);
     }
 
     /**

commit 277fe3f8c98060817cc3fd7bb2118c9ff6714a78

mvc: account for option container and passthrough value (#9174)
Fixes minor regression caused by 700d1d6

src/opnsense/mvc/app/models/OPNsense/Base/FieldTypes/BaseListField.php
@@ -136,7 +136,8 @@ abstract class BaseListField extends BaseField
     {
         $items = [];
         foreach ($this->getValues() as $item) {
-            $items[] = $this->internalOptionList[$item] ?? $item;
+            $val = $this->internalOptionList[$item] ?? $item;
+            $items[] = is_array($val) && isset($val['value']) ? $val['value'] : $val;
         }
         return implode(', ', $items);
     }