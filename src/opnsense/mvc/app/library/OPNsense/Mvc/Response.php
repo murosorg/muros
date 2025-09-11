murosorg/muros

commit afbba6f0b71f405ead552da9aced1096a7d27ce4

mvc: stream output not properly cleansed when used in widget.
Since we sanitize api output to the webgui by default, for consistency we should probably do the same when handing streamed data.
Move htmlspecialchars() to Response class to increase readability and, similar to array data, always assume the browser requires escaped data by default.

reported by: Stanislav Fort of Aisle Research

src/opnsense/mvc/app/library/OPNsense/Mvc/Response.php
@@ -35,6 +35,7 @@ class Response
     private Headers $headers;
     private mixed $content = '';
     private bool $sent = false;
+    private bool $safe_output = false;
 
     public function __construct()
     {
@@ -46,9 +47,10 @@ class Response
         return $this->headers;
     }
 
-    public function setContent(mixed $content): void
+    public function setContent(mixed $content, bool $safe=false): void
     {
         $this->content = $content;
+        $this->safe_output = $safe;
     }
 
     public function getContent(): string
@@ -104,14 +106,25 @@ class Response
             while (ob_get_level() > 0) {
                 ob_end_flush();
             }
-            fpassthru($this->content);
+            if ($this->safe_output) {
+                fpassthru($this->content);
+            } else {
+                /* similar escaping tactic as used for array type output */
+                while (($buffer = fgets($this->content)) !== false) {
+                    echo htmlspecialchars($buffer, ENT_NOQUOTES);
+                }
+            }
             @fclose($this->content);
         } elseif (!empty($this->content)) {
             if (is_array($this->content)) {
-                echo json_encode($this->content);
+                $result = json_encode($this->content);
+                echo $this->safe_output ? $result : htmlspecialchars($result, ENT_NOQUOTES);
             } else {
+                /* XXX: assume safe content here, to prevent breaking existing callers.
+                        eventually we need to move the responsibility of marking data safe to the caller explicitly*/
                 echo $this->content;
             }
+
         }
 
         $this->sent = true;

src/opnsense/mvc/app/controllers/OPNsense/Base/ApiControllerBase.php
@@ -174,6 +174,7 @@ class ApiControllerBase extends ControllerRoot
      * @param array $params list of parameters
      * @param array $headers http headers to send before pushing data
      * @param int $poll_timeout poll timeout after connect
+     * @param bool $safe when safe, no escaping will be applied
      */
     protected function configdStream(
         $action,
@@ -181,7 +182,8 @@ class ApiControllerBase extends ControllerRoot
         $headers = [
             'Content-Type: application/json', 'Content-Transfer-Encoding: binary', 'Pragma: no-cache', 'Expires: 0'
         ],
-        $poll_timeout = 2
+        $poll_timeout = 2,
+        $safe = false
     ) {
         $response = (new Backend())->configdpStream($action, $params, $poll_timeout);
 
@@ -189,7 +191,7 @@ class ApiControllerBase extends ControllerRoot
             $parts = explode(':', $header, 2);
             $this->response->setHeader($parts[0], ltrim($parts[1]));
         }
-        $this->response->setContent($response);
+        $this->response->setContent($response, $safe || $this->isExternalClient());
     }
 
     /**
@@ -352,11 +354,7 @@ class ApiControllerBase extends ControllerRoot
         $data = $dispatcher->getReturnedValue();
         if (is_array($data)) {
             $this->response->setContentType('application/json', 'UTF-8');
-            if ($this->isExternalClient()) {
-                $this->response->setContent(json_encode($data));
-            } else {
-                $this->response->setContent(htmlspecialchars(json_encode($data), ENT_NOQUOTES));
-            }
+            $this->response->setContent($data, $this->isExternalClient());
         } elseif (is_string($data)) {
             // XXX: fallback, controller returned data as string. a deprecation message might be an option here.
             $this->response->setContent($data);

src/opnsense/mvc/app/controllers/OPNsense/Base/ControllerBase.php
@@ -148,7 +148,7 @@ class ControllerBase extends ControllerRoot
         $this->view->processRender('', '');
         $this->view->finish();
 
-        $this->response->setContent($this->view->getContent());
+        $this->response->setContent($this->view->getContent(), true);
     }
 
     /**

src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/Api/LogController.php
@@ -90,7 +90,8 @@ class LogController extends ApiControllerBase
                         'Pragma: no-cache',
                         'Expires: 0'
                     ],
-                    60
+                    60,
+                    true
                 );
             } elseif ($action == "live") {
                 $offset = $this->request->get('offset', 'int', 0);

commit c59e27b4d825f19efa5da9b0a48e08da2bcbabb6

mvc: allow empty responses ([]), minor regression in https://github.com/opnsense/core/commit/afbba6f0b71f405ead552da9aced1096a7d27ce4 causing crashing widgets
closes https://github.com/opnsense/core/issues/9191

src/opnsense/mvc/app/library/OPNsense/Mvc/Response.php
@@ -115,11 +115,11 @@ class Response
                 }
             }
             @fclose($this->content);
-        } elseif (!empty($this->content)) {
+        } else {
             if (is_array($this->content)) {
                 $result = json_encode($this->content);
                 echo $this->safe_output ? $result : htmlspecialchars($result, ENT_NOQUOTES);
-            } else {
+            } elseif (!empty($this->content)) {
                 /* XXX: assume safe content here, to prevent breaking existing callers.
                         eventually we need to move the responsibility of marking data safe to the caller explicitly*/
                 echo $this->content;