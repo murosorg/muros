murosorg/muros

commit 218169c95c7e008e34189ed18259478954fb7fa2

bootgrid: allow conditional command rendering through a filter function
A "filter" key can be provided with an associated function that
return true or false depending on whether it should be rendered.
We pass the cell object so all relevant information is exposed.

While here, do a thorough cleanup of the _wireCommands function, as
this was triggered unnecessarily often.

src/opnsense/www/js/opnsense_bootgrid.js
@@ -856,9 +856,6 @@ class UIBootgrid {
             this.table.redraw();
         }
 
-        // DOM layout changed, rewire commands
-        this._wireCommands();
-
         if (this.table.getData().length == 0 && !this.loading) {
             this.dataAvailable = false;
             this._getPlaceholder().html(this.translations.noresultsfound);
@@ -913,7 +910,7 @@ class UIBootgrid {
                 let commandClass = classes.find(c => c.startsWith('command-'));
                 if (commandClass) {
                     let command = commandClass.replace('command-', '');
-                    this._wireCommands($(el), command);
+                    this._wireCommand($(el), command);
                 }
             })
         }
@@ -951,33 +948,27 @@ class UIBootgrid {
         });
     }
 
-    _wireCommands($selector=null, command=null) {
+    _wireCommand($selector=null, command=null, cell=null) {
         const commands = this._getCommands();
-        Object.keys(commands).map((k) => {
+        if ($selector && command && command in commands) {
             let has_option = true;
-            for (let i = 0; i < commands[k]['requires'].length; i++) {
-                if (!(commands[k]['requires'][i] in this.crud)) {
+            for (let i = 0; i < commands[command]['requires'].length; i++) {
+                if (!(commands[command]['requires'][i] in this.crud)) {
                     has_option = false;
                 }
             }
+
             if (has_option) {
-                // in both cases, make sure to stop the event from bubbling up
+                // make sure to stop the event from bubbling up
                 // to the parent so no handlers on parent containers are executed
-                if ($selector && command && command === k) {
-                    $selector.unbind('click').on("click", function (event) {
-                        event.stopPropagation();
-                        commands[k].method.bind(this)(event);
-                    });
-                } else {
-                    this.$element.find(".command-" + k).unbind('click').on("click", function (event) {
-                        event.stopPropagation();
-                        commands[k].method.bind(this)(event);
-                    });
-                }
-            } else if ($selector === null && $(".command-" + k).length > 0) {
-                console.log("not all requirements met to link " + k);
+                $selector.unbind('click').on("click", function (event) {
+                    event.stopPropagation();
+                    commands[command].method.bind(this)(event);
+                });
+            } else if ($(".command-" + command).length > 0) {
+                console.log("not all requirements met to link " + command);
             }
-        });
+        }
     }
 
     /**
@@ -1589,6 +1580,10 @@ class UIBootgrid {
                         if (!(command.requires[i] in this.crud)) {
                             has_option = false;
                         }
+                    }
+
+                    if ('filter' in command) {
+                        has_option = command.filter(cell);
                     }
 
                     if (has_option) {

commit 08ab1d0f8b423828d0a6296547c6def3d02c9cb9

bootgrid: separate global commands from per-row commands through a global toggle
This makes sure these are treated separately in the right place and at the right time.

Fixes unbound commands caused by https://github.com/opnsense/core/commit/218169c95c7e008e34189ed18259478954fb7fa2

src/opnsense/www/js/opnsense_bootgrid.js
@@ -851,6 +851,8 @@ class UIBootgrid {
 
         this.normalizeRowHeight();
 
+        this._wireGlobalCommands();
+
         if (this.options.virtualDOM) {
             // redraw here to prevent pagination switches from breaking the virtualdom rendering process
             this.table.redraw();
@@ -910,7 +912,7 @@ class UIBootgrid {
                 let commandClass = classes.find(c => c.startsWith('command-'));
                 if (commandClass) {
                     let command = commandClass.replace('command-', '');
-                    this._wireCommand($(el), command);
+                    this._wireCellCommand($(el), command);
                 }
             })
         }
@@ -948,9 +950,9 @@ class UIBootgrid {
         });
     }
 
-    _wireCommand($selector=null, command=null) {
+    _wireCellCommand($selector, command) {
         const commands = this._getCommands();
-        if ($selector && command && command in commands) {
+        if (command in commands) {
             let has_option = true;
             for (let i = 0; i < commands[command]['requires'].length; i++) {
                 if (!(commands[command]['requires'][i] in this.crud)) {
@@ -965,10 +967,29 @@ class UIBootgrid {
                     event.stopPropagation();
                     commands[command].method.bind(this)(event);
                 });
-            } else if ($(".command-" + command).length > 0) {
-                console.log("not all requirements met to link " + command);
             }
         }
+    }
+
+    _wireGlobalCommands() {
+        const commands = Object.fromEntries(Object.entries(this._getCommands()).filter(([key, value]) => value?.global));
+        Object.keys(commands).map((k) => {
+            let has_option = true;
+            for (let i = 0; i < commands[k]['requires'].length; i++) {
+                if (!(commands[k]['requires'][i] in this.crud)) {
+                    has_option = false;
+                }
+            }
+
+            if (has_option) {
+                this.$element.find(".command-" + k).unbind('click').on("click", function (event) {
+                    event.stopPropagation();
+                    commands[k].method.bind(this)(event);
+                });
+            } else if ($(".command-" + k).length > 0) {
+                console.log("not all requirements met to link " + k);
+            }
+        });
     }
 
     /**
@@ -1489,7 +1510,8 @@ class UIBootgrid {
             "add": {
                 method: this.command_add.bind(this),
                 requires: ['get', 'set'],
-                sequence: 100
+                sequence: 100,
+                global: true
             },
             "edit": {
                 method: this.command_edit.bind(this),
@@ -1523,7 +1545,8 @@ class UIBootgrid {
             "delete-selected": {
                 method: this.command_delete_selected.bind(this),
                 requires: ['del'],
-                sequence: 100
+                sequence: 100,
+                global: true
             }
         };
         // register additional commands