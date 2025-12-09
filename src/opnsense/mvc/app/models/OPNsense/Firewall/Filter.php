murosorg/muros

commit 449246c44e9ae0a5bca245812752a568e0bf1a6b

mvc: a bit more of asInt() and friends

src/opnsense/mvc/app/models/OPNsense/Firewall/Filter.php
@@ -203,18 +203,16 @@ class Filter extends BaseModel
                             $rule->adaptivestart->__reference
                         ));
                     } elseif (
-                        !empty((string)$rule->max) &&
-                        !empty((string)$rule->adaptiveend) &&
-                        (int)$rule->max->getValue() > (int)$rule->adaptiveend->getValue()
+                        !$rule->max->isEmpty() && !$rule->adaptiveend->isEmpty() &&
+                        $rule->max->asInt() > $rule->adaptiveend->asInt()
                     ) {
                         $messages->appendMessage(new Message(
                             gettext("The value of adaptive.end must be greater than the Max states value."),
                             $rule->adaptiveend->__reference
                         ));
                     } elseif (
-                        !empty((string)$rule->adaptivestart) &&
-                        !empty((string)$rule->adaptiveend) &&
-                        (int)$rule->adaptivestart->getValue() > (int)$rule->adaptiveend->getValue()
+                        !$rule->adaptivestart->isEmpty() && !$rule->adaptiveend->isEmpty() &&
+                        $rule->adaptivestart->asInt() > $rule->adaptiveend->asInt()
                     ) {
                         $messages->appendMessage(new Message(
                             gettext("The value of adaptive.end must be greater than adaptive.start value."),

src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
@@ -82,11 +82,11 @@ class FilterController extends FilterBaseController
         $categories = $this->request->get('category');
         $show_all = !empty($this->request->get('show_all'));
         if (!empty($this->request->get('interface'))) {
-            $interfaces = explode(",", $this->request->get('interface'));
+            $interfaces = explode(',', $this->request->get('interface'));
             /* add groups which contain the selected interface */
             foreach ((new Group())->ifgroupentry->iterateItems() as $groupItem) {
-                if (array_intersect($interfaces, explode(',', (string)$groupItem->members))) {
-                    $interfaces[] = (string)$groupItem->ifname;
+                if (array_intersect($interfaces, $groupItem->members->getValues())) {
+                    $interfaces[] = $groupItem->ifname->getValue();
                 }
             }
         } else {
@@ -96,7 +96,7 @@ class FilterController extends FilterBaseController
         /* filter logic for mvc rules */
         $filter_funct_mvc = function ($record) use ($categories, $interfaces, $show_all) {
             $is_cat = empty($categories) || array_intersect(explode(',', $record->categories), $categories);
-            $rule_interfaces = array_filter(explode(',', (string)$record->interface));
+            $rule_interfaces = $record->interface->getValues();
 
             if (empty($interfaces)) {
                 $is_if = count($rule_interfaces) != 1;
@@ -229,15 +229,17 @@ class FilterController extends FilterBaseController
 
     public function getRuleAction($uuid = null)
     {
-        $result = $this->getBase("rule", "rules.rule", $uuid);
+        $result = $this->getBase('rule', 'rules.rule', $uuid);
+
         if ($this->request->get('fetchmode') === 'copy' && !empty($result['rule'])) {
             /* copy mode, generate new sequence at the end */
             $max = 0;
             foreach ($this->getModel()->rules->rule->iterateItems() as $rule) {
-                $max = (int)((string)$rule->sequence) > $max ? (int)((string)$rule->sequence) : $max;
+                $max = max($rule->sequence->asInt(), $max);
             }
             $result['rule']['sequence'] = $max + 100;
         }
+
         return $result;
     }
 
@@ -399,7 +401,7 @@ class FilterController extends FilterBaseController
         // Count rules per interface
         $ruleCounts = [];
         foreach ((new \OPNsense\Firewall\Filter())->rules->rule->iterateItems() as $rule) {
-            $interfaces = array_filter(explode(',', (string)$rule->interface));
+            $interfaces = $rule->interface->getValues();
 
             if (count($interfaces) !== 1) {
                 // floating: empty or multiple interfaces

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/FilterRuleField.php
@@ -107,8 +107,8 @@ class FilterRuleContainerField extends ContainerField
     public function getPriority()
     {
         $configObj = Config::getInstance()->object();
-        $interface = (string)$this->interface;
-        if (strpos($interface, ",") !== false || empty($interface)) {
+        $interface = $this->interface->getValue();
+        if (strpos($interface, ',') !== false || empty($interface)) {
             // floating (multiple interfaces involved)
             return 200000;
         } elseif (
@@ -120,8 +120,8 @@ class FilterRuleContainerField extends ContainerField
             if (static::$ifgroups === null) {
                 static::$ifgroups = [];
                 foreach ((new Group())->ifgroupentry->iterateItems() as $node) {
-                    if (!empty((string)$node->sequence)) {
-                        static::$ifgroups[(string)$node->ifname] =  (int)((string)$node->sequence);
+                    if (!$node->sequence->isEmpty()) {
+                        static::$ifgroups[$node->ifname->getValue()] = $node->sequence->asInt();
                     }
                 }
             }

src/opnsense/mvc/app/models/OPNsense/Kea/KeaCtrlAgent.php
@@ -38,7 +38,7 @@ class KeaCtrlAgent extends BaseModel
         $cnf = [
             'Control-agent' => [
                 'http-host' => $this->general->http_host->getValue(),
-                'http-port' => (int)$this->general->http_port->getValue(),
+                'http-port' => $this->general->http_port->asInt(),
                 'control-sockets' => [
                     'dhcp4' => [
                         'socket-type' => 'unix',

src/opnsense/mvc/app/models/OPNsense/TrafficShaper/TrafficShaper.php
@@ -50,7 +50,7 @@ class TrafficShaper extends BaseModel
             if ($validateFullModel || $node->isFieldChanged()) {
                 $parentNode = $node->getParentNode();
                 if (in_array($node->getInternalXMLTagName(), ['bandwidth', 'bandwidthMetric'])) {
-                    $currentval = (int)(string)$parentNode->bandwidth;
+                    $currentval = $parentNode->bandwidth->asInt();
                     $maximumval = $max_bandwidth;
                     if ($parentNode->bandwidthMetric == "Kbit") {
                         $maximumval /= 1000;

commit 466a89b4bad5f671d8d1b5c3718f46a0f0a83de1

Firewall - fix regression in https://github.com/opnsense/core/commit/449246c44e9ae0a5bca245812752a568e0bf1a6b caused by fields not available in both snat and rules.
This commit re-arranges the existing validations so the type specific ones are only evaluated on that type.

src/opnsense/mvc/app/models/OPNsense/Firewall/Filter.php
@@ -112,24 +112,6 @@ class Filter extends BaseModel
                         ));
                     }
 
-                    // Additional source nat validations
-                    if ($rule->target !== null) {
-                        $target_is_addr = Util::isSubnet($rule->target) || Util::isIpAddress($rule->target);
-                        $target_proto = strpos($rule->target, ':') === false ? "inet" : "inet6";
-                        if ($target_is_addr && $target_proto != $rule->ipprotocol) {
-                            $messages->appendMessage(new Message(
-                                gettext("Target address type should match selected TCP/IP protocol version."),
-                                $rule->target->__reference
-                            ));
-                        }
-                        if (!empty((string)$rule->target_port) && !in_array($rule->protocol, $port_protos)) {
-                            $messages->appendMessage(new Message(
-                                gettext("Target ports are only valid for tcp or udp type rules."),
-                                $rule->target_port->__reference
-                            ));
-                        }
-                    }
-
                     if (
                         !empty((string)$rule->interfacenot) && (
                         count(explode(',', $rule->interface)) != 1 || empty((string)$rule->interface)
@@ -140,21 +122,6 @@ class Filter extends BaseModel
                                 "single targets to avoid mis-interpretations"),
                             $rule->interfacenot->__reference
                         ));
-                    }
-                    if ($rule->statetype == 'none') {
-                        foreach (
-                            [
-                            'statetimeout', 'max', 'max-src-states', 'max-src-nodes', 'adaptivestart', 'adaptiveend',
-                            'max-src-conn'
-                            ] as $fieldname
-                        ) {
-                            if (!empty((string)$rule->$fieldname)) {
-                                $messages->appendMessage(new Message(
-                                    gettext("Invalid option when statetype is none."),
-                                    $rule->$fieldname->__reference
-                                ));
-                            }
-                        }
                     }
                     if (!in_array($rule->protocol, ['TCP', 'TCP/UDP'])) {
                         foreach (
@@ -171,76 +138,113 @@ class Filter extends BaseModel
                             }
                         }
                     }
-                    if (!empty((string)$rule->{'max-src-conn-rate'}) xor !empty((string)$rule->{'max-src-conn-rates'})) {
-                        $tmp = empty((string)$rule->{'max-src-conn-rate'}) ? 'max-src-conn-rate' : 'max-src-conn-rates';
-                        $messages->appendMessage(new Message(
-                            gettext("Need to specify both a number of connections and a time interval."),
-                            $rule->$tmp->__reference
-                        ));
-                    }
 
-                    if (!empty((string)$rule->tcpflags1) && empty((string)$rule->tcpflags2)) {
-                        $messages->appendMessage(new Message(
-                            gettext("If you specify TCP flags that should be set " .
-                                "you should specify out of which flags as well."),
-                            $rule->tcpflags2->__reference
-                        ));
-                    }
-                    if (empty((string)$rule->max) && ($rule->adaptivestart == '0' || $rule->adaptiveend == '0')) {
-                        $messages->appendMessage(new Message(
-                            gettext('Disabling adaptive timeouts is only supported in ".
-                                "combination with a configured maximum number of states for the same rule.'),
-                            $rule->max->__reference
-                        ));
-                    } elseif ($rule->adaptivestart == '0' xor $rule->adaptiveend == '0') {
-                        $messages->appendMessage(new Message(
-                            gettext("Adaptive timeouts must be disabled together."),
-                            $rule->adaptivestart->__reference
-                        ));
-                    } elseif (!empty((string)$rule->adaptivestart) xor !empty((string)$rule->adaptiveend)) {
-                        $messages->appendMessage(new Message(
-                            gettext("The adaptive timouts values must be set together."),
-                            $rule->adaptivestart->__reference
-                        ));
-                    } elseif (
-                        !$rule->max->isEmpty() && !$rule->adaptiveend->isEmpty() &&
-                        $rule->max->asInt() > $rule->adaptiveend->asInt()
-                    ) {
-                        $messages->appendMessage(new Message(
-                            gettext("The value of adaptive.end must be greater than the Max states value."),
-                            $rule->adaptiveend->__reference
-                        ));
-                    } elseif (
-                        !$rule->adaptivestart->isEmpty() && !$rule->adaptiveend->isEmpty() &&
-                        $rule->adaptivestart->asInt() > $rule->adaptiveend->asInt()
-                    ) {
-                        $messages->appendMessage(new Message(
-                            gettext("The value of adaptive.end must be greater than adaptive.start value."),
-                            $rule->adaptiveend->__reference
-                        ));
-                    }
-
-                    if ((string)$rule->{'set-prio'} == '' && (string)$rule->{'set-prio-low'} != '') {
-                        $messages->appendMessage(new Message(
-                            gettext("Set priority for low latency and acknowledgements " .
-                                "requires a set priority for normal packets."),
-                            $rule->{'set-prio-low'}->__reference
-                        ));
-                    }
-                    if (!empty((string)$rule->shaper1) || !empty((string)$rule->shaper2)) {
-                        if (!empty((string)$rule->shaper2) && empty((string)$rule->shaper1)) {
+                    /* rule type specific rules (filter, snat) */
+                    if ($rule->target !== null) {
+                        // Additional source nat validations
+                        $target_is_addr = Util::isSubnet($rule->target) || Util::isIpAddress($rule->target);
+                        $target_proto = strpos($rule->target, ':') === false ? "inet" : "inet6";
+                        if ($target_is_addr && $target_proto != $rule->ipprotocol) {
                             $messages->appendMessage(new Message(
-                                gettext("A shaper is required when configuring one in the reverse direction."),
-                                $rule->shaper2->__reference
+                                gettext("Target address type should match selected TCP/IP protocol version."),
+                                $rule->target->__reference
                             ));
-                        } elseif (!empty((string)$rule->shaper2)) {
-                            $tmp1 = $dntargets[(string)$rule->shaper1] ?? ['type' => '-'];
-                            $tmp2 = $dntargets[(string)$rule->shaper2] ?? ['type' => '-'];
-                            if ($tmp1['type'] != $tmp2['type']) {
+                        }
+                        if (!empty((string)$rule->target_port) && !in_array($rule->protocol, $port_protos)) {
+                            $messages->appendMessage(new Message(
+                                gettext("Target ports are only valid for tcp or udp type rules."),
+                                $rule->target_port->__reference
+                            ));
+                        }
+                    } else {
+                        // Additional filter validations
+                        if (empty((string)$rule->max) && ($rule->adaptivestart == '0' || $rule->adaptiveend == '0')) {
+                            $messages->appendMessage(new Message(
+                                gettext('Disabling adaptive timeouts is only supported in ".
+                                    "combination with a configured maximum number of states for the same rule.'),
+                                $rule->max->__reference
+                            ));
+                        } elseif ($rule->adaptivestart == '0' xor $rule->adaptiveend == '0') {
+                            $messages->appendMessage(new Message(
+                                gettext("Adaptive timeouts must be disabled together."),
+                                $rule->adaptivestart->__reference
+                            ));
+                        } elseif (!empty((string)$rule->adaptivestart) xor !empty((string)$rule->adaptiveend)) {
+                            $messages->appendMessage(new Message(
+                                gettext("The adaptive timouts values must be set together."),
+                                $rule->adaptivestart->__reference
+                            ));
+                        } elseif (
+                            !$rule->max->isEmpty() && !$rule->adaptiveend->isEmpty() &&
+                            $rule->max->asInt() > $rule->adaptiveend->asInt()
+                        ) {
+                            $messages->appendMessage(new Message(
+                                gettext("The value of adaptive.end must be greater than the Max states value."),
+                                $rule->adaptiveend->__reference
+                            ));
+                        } elseif (
+                            !$rule->adaptivestart->isEmpty() && !$rule->adaptiveend->isEmpty() &&
+                            $rule->adaptivestart->asInt() > $rule->adaptiveend->asInt()
+                        ) {
+                            $messages->appendMessage(new Message(
+                                gettext("The value of adaptive.end must be greater than adaptive.start value."),
+                                $rule->adaptiveend->__reference
+                            ));
+                        }
+                        if ($rule->statetype == 'none') {
+                            foreach (
+                                [
+                                'statetimeout', 'max', 'max-src-states', 'max-src-nodes', 'adaptivestart', 'adaptiveend',
+                                'max-src-conn'
+                                ] as $fieldname
+                            ) {
+                                if (!empty((string)$rule->$fieldname)) {
+                                    $messages->appendMessage(new Message(
+                                        gettext("Invalid option when statetype is none."),
+                                        $rule->$fieldname->__reference
+                                    ));
+                                }
+                            }
+                        }
+
+                        if (!empty((string)$rule->{'max-src-conn-rate'}) xor !empty((string)$rule->{'max-src-conn-rates'})) {
+                            $tmp = empty((string)$rule->{'max-src-conn-rate'}) ? 'max-src-conn-rate' : 'max-src-conn-rates';
+                            $messages->appendMessage(new Message(
+                                gettext("Need to specify both a number of connections and a time interval."),
+                                $rule->$tmp->__reference
+                            ));
+                        }
+
+                        if (!empty((string)$rule->tcpflags1) && empty((string)$rule->tcpflags2)) {
+                            $messages->appendMessage(new Message(
+                                gettext("If you specify TCP flags that should be set " .
+                                    "you should specify out of which flags as well."),
+                                $rule->tcpflags2->__reference
+                            ));
+                        }
+
+                        if ((string)$rule->{'set-prio'} == '' && (string)$rule->{'set-prio-low'} != '') {
+                            $messages->appendMessage(new Message(
+                                gettext("Set priority for low latency and acknowledgements " .
+                                    "requires a set priority for normal packets."),
+                                $rule->{'set-prio-low'}->__reference
+                            ));
+                        }
+                        if (!empty((string)$rule->shaper1) || !empty((string)$rule->shaper2)) {
+                            if (!empty((string)$rule->shaper2) && empty((string)$rule->shaper1)) {
                                 $messages->appendMessage(new Message(
-                                    gettext("Pipes and queues can not be combined."),
+                                    gettext("A shaper is required when configuring one in the reverse direction."),
                                     $rule->shaper2->__reference
                                 ));
+                            } elseif (!empty((string)$rule->shaper2)) {
+                                $tmp1 = $dntargets[(string)$rule->shaper1] ?? ['type' => '-'];
+                                $tmp2 = $dntargets[(string)$rule->shaper2] ?? ['type' => '-'];
+                                if ($tmp1['type'] != $tmp2['type']) {
+                                    $messages->appendMessage(new Message(
+                                        gettext("Pipes and queues can not be combined."),
+                                        $rule->shaper2->__reference
+                                    ));
+                                }
                             }
                         }
                     }