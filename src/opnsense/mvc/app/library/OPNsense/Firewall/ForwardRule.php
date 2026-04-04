murosorg/muros

commit 57add7fff54c1472923738018ad740d0af42ee48

Firewall: NAT: Destination NAT - change "pass" into an option field, which chooses between "manual" (do nothing), add a pass flag or register a rule at the bottom of our interface ruleset.
Should be the last piece of the puzzle for https://github.com/opnsense/core/issues/8401

src/opnsense/mvc/app/library/OPNsense/Firewall/ForwardRule.php
@@ -100,7 +100,7 @@ class ForwardRule extends Rule
         foreach ($this->reader() as $tmp) {
             $tmp['rule_types'] = array("rdr");
             $tmp['nordr'] = !empty($tmp['nordr']);
-            if (!empty($tmp['pass'])) {
+            if (!empty($tmp['pass']) && $tmp['pass'] == 'pass') {
                 $tmp['pass'] = empty($tmp['nordr']);
             }
             // target address, when invalid, disable rule

src/etc/inc/filter.lib.inc
@@ -636,6 +636,35 @@ function filter_core_rules_system($fw, $defaults)
             }
         }
     }
+
+    // auto-generated DNAT rules
+    foreach ((new OPNsense\Firewall\DNat(False))->rule->sortedBy(['sequence']) as $key => $rule) {
+        if ($rule->disabled->isEmpty() && $rule->pass == 'rule') {
+            $dnatrule = $rule->getNodeContent();
+            $tmprule = [
+                'ipprotocol' =>  $dnatrule['ipprotocol'],
+                'interface' => $dnatrule['interface'],
+                'protocol' =>  $dnatrule['protocol'],
+                'source' => $dnatrule['source'],
+                'destination' => [
+                    'network' => $dnatrule['target']
+                ],
+                'log' => $dnatrule['log'],
+                'descr' => $dnatrule['descr']
+            ];
+
+            if (!empty($dnatrule['destination']['port']) && !empty($dnatrule['local-port'])) {
+                $tmprule['destination']['port'] = $dnatrule['local-port'];
+                $parts = explode('-', $dnatrule['destination']['port']);
+                if (count($parts) == 2) {
+                    $range_end = $parts[1] - $parts[0] +  $dnatrule['local-port'];
+                    $dnatrule['local-port'] .= sprint("-%s", $range_end);
+                }
+            }
+            $fw->registerFilterRule(500000, $tmprule); /* automatic at the end of our ruleset */
+        }
+    }
+
 }
 
 /**

src/opnsense/mvc/app/controllers/OPNsense/Firewall/forms/dialogDNatRule.xml
@@ -262,9 +262,9 @@
     </field>
     <field>
         <id>rule.pass</id>
-        <label>Automatically pass</label>
-        <type>checkbox</type>
-        <help>When this checkbox is set, traffic matching this nat rule will automatically be passed without inspecting firewall rules. We do advise to create rules manually for better visibility, please keep in mind the destination for the rule should match the target defined in this NAT rule.</help>
+        <label>Firewall rule</label>
+        <type>dropdown</type>
+        <help>By default, firewall rules need to be created manually, which is also the advised option. Alternatively you can use Pass, which passes traffic on the nat rule (not visible in the rules tab) or generate interface rules which can be overruled via rules with a higher priority. Please keep in mind the destination for the rule should match the target defined in this NAT rule.</help>
         <grid_view>
             <visible>false</visible>
         </grid_view>

src/opnsense/mvc/app/models/OPNsense/Firewall/DNat.xml
@@ -98,7 +98,13 @@
                     <disable>Disable</disable>
                 </OptionValues>
             </natreflection>
-            <pass type="BooleanField"/>
+            <pass type="OptionField">
+                <BlankDesc>Manual</BlankDesc>
+                <OptionValues>
+                    <rule>Register rule</rule>
+                    <pass>Pass</pass>
+                </OptionValues>
+            </pass>
             <!-- drop in a future version (27.1?), used for migration purposes.-->
             <associated-rule-id type=".\DNatAssociatedRuleField"/>
             <!-- handled via controller setBaseHook() -->

src/opnsense/mvc/app/models/OPNsense/Firewall/FieldTypes/DNatAssociatedRuleField.php
@@ -40,7 +40,7 @@ class DNatAssociatedRuleField extends NetworkAliasField
     public function setValue($value)
     {
         if (is_a($value, \SimpleXMLElement::class) && $value == 'pass') {
-            $this->getParentNode()->pass = '1';
+            $this->getParentNode()->pass = 'pass';
         }
         /* unused, flush */
         return parent::setValue('');

commit bb5300054631a7cb595a3f9a737ead8fff90b6c8

Firewall: NAT: Destination NAT - regression in https://github.com/opnsense/core/commit/57add7fff54c1472923738018ad740d0af42ee48, closes https://github.com/opnsense/core/issues/9702

src/opnsense/mvc/app/library/OPNsense/Firewall/ForwardRule.php
@@ -100,8 +100,8 @@ class ForwardRule extends Rule
         foreach ($this->reader() as $tmp) {
             $tmp['rule_types'] = array("rdr");
             $tmp['nordr'] = !empty($tmp['nordr']);
-            if (!empty($tmp['pass']) && $tmp['pass'] == 'pass') {
-                $tmp['pass'] = empty($tmp['nordr']);
+            if (!empty($tmp['pass'])) {
+                $tmp['pass'] = empty($tmp['nordr']) && $tmp['pass'] != 'rule';
             }
             // target address, when invalid, disable rule
             if (empty($tmp['target'])) {