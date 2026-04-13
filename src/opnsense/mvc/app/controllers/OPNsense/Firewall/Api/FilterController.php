murosorg/muros

commit f0bb9f7b1394c84d85b24f6f3daadc651475dfa1

mvc: minor cleanup in UIModelGrid, remove flatten() method as getFlatNodes() is almost the same.

src/opnsense/mvc/app/library/OPNsense/Base/UIModelGrid.php
@@ -114,7 +114,7 @@ class UIModelGrid
         $fields,
         $itemsPerPage,
         $currentPage,
-        $sortBy = array(),
+        $sortBy = [],
         $sortDescending = false,
         $search_tokens = [],
         $filter_funct = null,
@@ -132,7 +132,11 @@ class UIModelGrid
 
                 // parse rows, because we may need to convert some (list) items we need to know the actual content
                 // before searching we flatten the resulting array in case of nested containers
-                $row = iterator_to_array($this->flatten(array_merge(['uuid' => $record->getAttributes()['uuid']], $record->getNodeContent())));
+                $row = ['uuid' => $record->getAttributes()['uuid']];
+                $reflen = strlen($record->__reference) + 1;
+                foreach ($record->getFlatNodes() as $key => $val) {
+                   $row[substr($key, $reflen)] = $val->getValue();
+                }
 
                 // if search tokens are provided, use them to search in all requested fields
                 if (!empty($search_tokens)) {
@@ -171,16 +175,5 @@ class UIModelGrid
         $result['current'] = (int)$currentPage;
 
         return $result;
-    }
-
-    private function flatten($node, $path = '')
-    {
-        if (is_array($node)) {
-            foreach ($node as $key => $value) {
-                yield from $this->flatten($value, ltrim($path . '.' . $key, '.'));
-            }
-        } else {
-            yield $path => $node;
-        }
     }
 }

commit 6169e9f66118551f04229cf5c5bbd55121edfdfe

mvc: regression in f0bb9f7b1394c84d85b24f6f3daadc651475dfa1, missing descriptions.

src/opnsense/mvc/app/controllers/OPNsense/Firewall/Api/FilterController.php
@@ -104,7 +104,12 @@ class FilterController extends FilterBaseController
             $row = ['uuid' => $record->getAttributes()['uuid']];
             $reflen = strlen($record->__reference) + 1;
             foreach ($record->getFlatNodes() as $key => $val) {
-                $row[substr($key, $reflen)] = $val->getValue();
+                $fieldname = substr($key, $reflen);
+                $descr = $val->getDescription();
+                $row[$fieldname] = $val->getValue();
+                if ($row[$fieldname] != $descr) {
+                    $row['%' . $fieldname] = $descr;
+                }
             }
             $allrules[] = $row;
         }

src/opnsense/mvc/app/library/OPNsense/Base/UIModelGrid.php
@@ -135,7 +135,12 @@ class UIModelGrid
                 $row = ['uuid' => $record->getAttributes()['uuid']];
                 $reflen = strlen($record->__reference) + 1;
                 foreach ($record->getFlatNodes() as $key => $val) {
-                    $row[substr($key, $reflen)] = $val->getValue();
+                    $fieldname = substr($key, $reflen);
+                    $descr = $val->getDescription();
+                    $row[$fieldname] = $val->getValue();
+                    if ($row[$fieldname] != $descr) {
+                        $row['%' . $fieldname] = $descr;
+                    }
                 }
 
                 // if search tokens are provided, use them to search in all requested fields